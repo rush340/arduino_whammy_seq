a642fa3299a1f4a2d1dd43a7a4dc932a1ad90a3e
Michael Silver <rush340@gmail.com>
2014-10-20 15:18:08 -0700

    whammy position MIDI function implemented removed LED code

--- whammy_seq.ino
+++ whammy_seq.ino
@@ -12,38 +12,35 @@ int active_program = 0; // stored program # 0-3
 int current_step = 0;
 
 // sequence will eventually be loaded from EEPROM
-int sequence[] = {0, 127, 0, 127, 0, 127, 0, 127};
+int sequence[] = {0, 127};
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  pinMode(13, OUTPUT);
+  //  Set MIDI baud rate:
+  Serial.begin(31250);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  blink();
-
   // there will eventually be loops for "stop" and "program" modes
   play_loop();
 }
 
-void blink() {
-  // blink built-in LED to show program is running
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(50);
-}
-
 void play_loop() {
-  send_whammy_position(sequence[current_step]);
+  send_whammy_position((byte) sequence[current_step]);
+
+  // TODO get rid of delay and use millis()
+  delay(250);
 
   // next step
   current_step += 1;
   current_step = current_step % (sizeof(sequence) / sizeof(sequence[0]));
 }
 
-void send_whammy_position(int value) {
-  // TODO
+void send_whammy_position(byte value) {
+  Serial.write((byte) 176); // CC on Channel 1 (10110000)
+  Serial.write((byte) 11); // Controller 11
+  Serial.write(value); // value
 }
+
